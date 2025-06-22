1d22c029b356e2c1f462e5c5ef5bfc91ec9db4df
MDirovvvv <mishkadirovv2.0@gmail.com>
2025-06-23 00:32:58 +0300

    Добавьте файлы проекта.

---
+++ ComputersAuditorium/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using ComputersAuditorium.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputersAuditorium
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Auditorium> Auditoriums { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ComputersAuditorium.db");
+        }
+    }
+}
